619562073e44e7f74694d3bb7a00912ea9b0d97f
Kaeneth Dayao <kendayao@gmail.com>
2020-04-26 19:53:19 -0700

    added answer status

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ var questions = [
 
       {question: "<h3>The condition in an if/else statement is enclosed within ___.",
         answer1: "<button>a. quotes</button>",
-        answer2: "<button>b.curly brackets</button>",
+        answer2: "<button>b. curly brackets</button>",
         answer3: "<button>c. parentheses</button>",
-        answer4: "<button>d.square brackets</button>",
-        correct: "b. parenetheses",
+        answer4: "<button>d. square brackets</button>",
+        correct: "b. curly brackets"
     },
 
         {question: "<h3>Arrays in Javascript can be used to store ___</h3>",
@@ -20,78 +20,196 @@ var questions = [
         answer2: "<button>b. other arrays</button>",
         answer3: "<button>c. booleans</button>",
         answer4: "<button>d. all of the above</button>",
-        correct: "d. all of the above",
+        correct: "d. all of the above"
+},
+        {   question: "<h3>Commonly used data types DO NOT include:</h3>",
+        answer1: "<button>a. strings</button>",
+        answer2: "<button>b. booleans</button>",
+        answer3: "<button>c. alerts</button>",
+        answer4: "<button>d. numbers</button>",
+        correct: "c. alerts"
+    },
+    {question: "<h3>Arrays in Javascript can be used to store ___</h3>",
+    answer1: "<button>a. numbers and strings</button>",
+    answer2: "<button>b. other arrays</button>",
+    answer3: "<button>c. booleans</button>",
+    answer4: "<button>d. all of the above</button>",
+    correct: "d. all of the above"
     }]
+    
 
     var timer = 75;
     var questionAnswer=document.body.querySelector("#question-container");
     var startButton=document.body.querySelector(".start-button");
 
+    
+   
+
   startButton.addEventListener("click", function(){
 
-    setInterval(function(){
+    setInterval(function(event){
         timer--;
         document.body.querySelector("#timeclock").textContent= timer;
     }, 1000);
     
-    questionAnswer.innerHTML= questions[0].question;
+    indexOfQuestion = 0;
+    showQuestion(indexOfQuestion);
+});
+
+    function showQuestion(a){
+    questionAnswer.innerHTML= questions[a].question;
     var oLEl = document.createElement("ol");
     questionAnswer.appendChild(oLEl);
-    oLEl.setAttribute("style", "list-style-type: none")
+    oLEl.setAttribute("style", "list-style-type: none", "text-align:center")
+    oLEl.innerHTML ="";
     var liEL1 = document.createElement("li")
-    liEL1.innerHTML = questions[0].answer1;
-    oLEl. appendChild(liEL1);
+    liEL1.innerHTML = questions[a].answer1;
+    oLEl.appendChild(liEL1);
     var liEL2 = document.createElement("li")
-    liEL2.innerHTML = questions[0].answer2;
-    oLEl. appendChild(liEL2);
+    liEL2.innerHTML = questions[a].answer2;
+    oLEl.appendChild(liEL2);
     var liEL3 = document.createElement("li")
-    liEL3.innerHTML = questions[0].answer3;
-    oLEl. appendChild(liEL3);
+    liEL3.innerHTML = questions[a].answer3;
+    oLEl.appendChild(liEL3);
     var liEL4 = document.createElement("li")
-    liEL4.innerHTML = questions[0].answer4;
-    oLEl. appendChild(liEL4);
-    
-    oLEl.addEventListener("click",function(){
-        questionAnswer.innerHTML= "";
-        questionAnswer.innerHTML= questions[1].question;
-        
-        questionAnswer.appendChild(oLEl);
-        oLEl.setAttribute("style", "list-style-type: none")
-        oLEl.innerHTML ="";
-        var liEL1 = document.createElement("li")
-        liEL1.innerHTML = questions[1].answer1;
-        oLEl. appendChild(liEL1);
-        var liEL2 = document.createElement("li")
-        liEL2.innerHTML = questions[1].answer2;
-        oLEl. appendChild(liEL2);
-        var liEL3 = document.createElement("li")
-        liEL3.innerHTML = questions[1].answer3;
-        oLEl. appendChild(liEL3);
-        var liEL4 = document.createElement("li")
-        liEL4.innerHTML = questions[1].answer4;
-        oLEl. appendChild(liEL4);
+    liEL4.innerHTML = questions[a].answer4;
+    oLEl.appendChild(liEL4);
 
-        oLEl.addEventListener("click",function(){
-            questionAnswer.innerHTML= "";
-            questionAnswer.innerHTML= questions[2].question;
+        
+   
+    oLEl.addEventListener("click",function(event){
+        console.log(event.target.innerHTML)
+        console.log(indexOfQuestion)
+        console.log(questions[indexOfQuestion].correct)
+        if(event.target.innerHTML===questions[indexOfQuestion].correct){
             
-            questionAnswer.appendChild(oLEl);
-            oLEl.setAttribute("style", "list-style-type: none")
-            oLEl.innerHTML ="";
-            var liEL1 = document.createElement("li")
-            liEL1.innerHTML = questions[2].answer1;
-            oLEl. appendChild(liEL1);
-            var liEL2 = document.createElement("li")
-            liEL2.innerHTML = questions[2].answer2;
-            oLEl. appendChild(liEL2);
-            var liEL3 = document.createElement("li")
-            liEL3.innerHTML = questions[2].answer3;
-            oLEl. appendChild(liEL3);
-            var liEL4 = document.createElement("li")
-            liEL4.innerHTML = questions[2].answer4;
-            oLEl. appendChild(liEL4);
-        });
+            document.body.querySelector("#answerStatus").innerHTML = "<h3>Correct!</h3>"
+            
+        } else {
+            document.body.querySelector("#answerStatus").innerHTML = "<h3>Incorrect!</h3>"
+            timer=timer-10;
+        }
+
+        indexOfQuestion++;
+       showQuestion(indexOfQuestion);
     });
-});
+}
+
+
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+//     questionAnswer.innerHTML= questions[0].question;
+//     var oLEl = document.createElement("ol");
+//     questionAnswer.appendChild(oLEl);
+//     oLEl.setAttribute("style", "list-style-type: none")
+//     var liEL1 = document.createElement("li")
+//     liEL1.innerHTML = questions[0].answer1;
+//     oLEl.appendChild(liEL1);
+//     var liEL2 = document.createElement("li")
+//     liEL2.innerHTML = questions[0].answer2;
+//     oLEl.appendChild(liEL2);
+//     var liEL3 = document.createElement("li")
+//     liEL3.innerHTML = questions[0].answer3;
+//     oLEl.appendChild(liEL3);
+//     var liEL4 = document.createElement("li")
+//     liEL4.innerHTML = questions[0].answer4;
+//     oLEl.appendChild(liEL4);
+    
+    
+//     oLEl.addEventListener("click",function(event){
+        
+//         questionAnswer.innerHTML= questions[1].question;
+//         questionAnswer.appendChild(oLEl);
+//         oLEl.setAttribute("style", "list-style-type: none")
+//         oLEl.innerHTML ="";
+//         var liEL1 = document.createElement("li")
+//         liEL1.innerHTML = questions[1].answer1;
+//         oLEl.appendChild(liEL1);
+//         var liEL2 = document.createElement("li")
+//         liEL2.innerHTML = questions[1].answer2;
+//         oLEl.appendChild(liEL2);
+//         var liEL3 = document.createElement("li")
+//         liEL3.innerHTML = questions[1].answer3;
+//         oLEl.appendChild(liEL3);
+//         var liEL4 = document.createElement("li")
+//         liEL4.innerHTML = questions[1].answer4;
+//         oLEl.appendChild(liEL4);
+
+
+//         oLEl.addEventListener("click",function(event){
+           
+//             questionAnswer.innerHTML= questions[2].question;
+//             questionAnswer.appendChild(oLEl);
+//             oLEl.setAttribute("style", "list-style-type: none")
+//             oLEl.innerHTML ="";
+//             var liEL1 = document.createElement("li")
+//             liEL1.innerHTML = questions[2].answer1;
+//             oLEl.appendChild(liEL1);
+//             var liEL2 = document.createElement("li")
+//             liEL2.innerHTML = questions[2].answer2;
+//             oLEl.appendChild(liEL2);
+//             var liEL3 = document.createElement("li")
+//             liEL3.innerHTML = questions[2].answer3;
+//             oLEl.appendChild(liEL3);
+//             var liEL4 = document.createElement("li")
+//             liEL4.innerHTML = questions[2].answer4;
+//             oLEl.appendChild(liEL4);
+
+    
+//          });
+        
+         
+//     });
+
+
+// });
+
+  
+// if (event.target.innerHTML === questions[0].correct){
+//     var h5El = document.createElement("h5");
+//     h5El.innerHTML ="Correct";
+//     questionAnswer.appendChild(h5El);
+//    }else {
+//     var h5El = document.createElement("h5");
+//     h5El.innerHTML ="Incorrect";
+//     questionAnswer.appendChild(h5El);
+//    }
 
 
